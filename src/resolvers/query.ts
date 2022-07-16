6c5307b2c5d6e0595e5efcdafd4e4f8e96848196
Chriscastanuela <chriscastanuela@ymail.com>
2022-07-16 17:13:23 -0600

    add the me query

--- src/resolvers/query.ts
+++ src/resolvers/query.ts
@@ -1,6 +1,21 @@
-import { Context } from "../index";
+import { Context } from "../";
 
 export const Query = {
+    me:(
+        _:any,
+        __:any,
+        {
+            userInfo,
+            prisma
+        }:Context
+    ) => {
+        if(!userInfo) return null;
+        return prisma.user.findUnique({
+            where:{
+                id:userInfo.userId,
+            }
+        })
+    },
     posts: (
         _:any,
         __:any,
